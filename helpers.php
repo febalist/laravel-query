6007cf30a6fb86e1fb5ace98477ce984d73cd3dc
febalist <febalist@gmail.com>
2021-12-13 02:29:16 +0200

    helpers

--- helpers.php
+++ helpers.php
@@ -1,1 +1,23 @@
 <?php
+
+use Spatie\Url\Url;
+
+function url_query($url, array $parameters = [])
+{
+    $url = Url::fromString($url);
+
+    foreach ($parameters as $key => $value) {
+        if ($value === null) {
+            $url = $url->withoutQueryParameter($key);
+        } else {
+            $url = $url->withQueryParameter($key, $value);
+        }
+    }
+
+    return (string) $url;
+}
+
+function query(array $parameters = [])
+{
+    return url_query(request()->fullUrl(), $parameters);
+}
